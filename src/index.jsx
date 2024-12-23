895c6c5b44e163d17811f38715ed952e24bf32a3
raygaeta <raymondgaeta@gmail.com>
2024-12-23 11:09:26 -0800

    orbit controls on mobile

--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,12 @@ import './style.css'
 import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
 import Experience from './Experience.jsx'
-import { KeyboardControls } from '@react-three/drei'
+import { KeyboardControls, OrbitControls } from '@react-three/drei'
 import Interface from './Interface.jsx'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
+
+const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent)
 
 root.render(
     <KeyboardControls
@@ -27,6 +29,7 @@ root.render(
             } }
         >
             <Experience />
+            {isMobile && <OrbitControls />}
         </Canvas>
         <Interface />
     </KeyboardControls>
